8c201c5f4670adf20dafea49ca232bd335653ef2
Your Name <you@example.com>
2022-04-23 22:30:58 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -64,6 +64,8 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
     }
     else if(tree->lower_than(key, tree->current->pair->key ) == 1){
         tree->current->left = new;
+        new->parent = tree->current;
+        tree->current = new;
     }
     }
 }
